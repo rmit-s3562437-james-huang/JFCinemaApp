78da890b0c7feb79128cb44179b4662fc062841a
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2017-08-25 19:06:20 +1000

    home segue to desc view

--- JFCinemaApp/DescViewController.swift
+++ JFCinemaApp/DescViewController.swift
@@ -10,5 +10,19 @@ import UIKit
 
 class DescViewController: UIViewController {
 
+    var selected: String!
+    
+    @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var contentClassification: UIImageView!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var classDesc: UILabel!
+    @IBOutlet weak var contentDesc: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        self.myTitle.text = selected!
+    }
     
 }
