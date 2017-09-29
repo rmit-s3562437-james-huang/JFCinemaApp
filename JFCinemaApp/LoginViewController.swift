a94114bb8ffb3df89bb74601c8501d5655cd3f6f
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2017-09-29 21:59:09 +1000

    added frontend login/reg UI

---
+++ JFCinemaApp/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  JFCinemaApp
+//
+//  Created by James Huang on 29/9/17.
+//  Copyright © 2017 James Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var usernameFieldText: UITextField!
+    @IBOutlet weak var passwordFieldText: UITextField!
+}
---
+++ JFCinemaApp/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  JFCinemaApp
+//
+//  Created by James Huang on 29/9/17.
+//  Copyright © 2017 James Huang. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
